ef8888231181a9821fd3efc64b24492792b2e14c
94hours <admin@94hours.org.ua>
2014-11-17 00:53:52 +0200

    first commit

--- test/tema.php
+++ test/tema.php
@@ -13,20 +13,20 @@ class riba {
 
     public function getName (){
         return $this->name;
-        }
+    }
 
     public function getSize (){
         return $this->size;
-        }
+    }
 
     public function getPlavnik (){
         return $this->plavnik;
-        }
+    }
 
     public function getColor (){
         return $this->color;
-        }
-   }
+    }
+}
 
 
 
@@ -120,24 +120,85 @@ class riba2 extends riba{
     }
 }
 
-//class ribina {
-//    private function lol ($name,$color,$size,$plavnik){
-//        $myArray [] = $name;
-//        $myArray [] = $color;
-//        $myArray [] = $size;
-//        $myArray [] = $plavnik;
-//
-//        return $myArray;
-//    }
-//    public function publicLol ($name,$color,$size,$plavnik){
-//        $dump = $this->lol($name,$color,$size,$plavnik);
-//        var_export($dump);
-//        var_export($dump['']);
-//       $tut = $dump[''];
-//        var_dump($tut);
-//        for($i = 0,  $count = count($dump) + 0; $i < $count; $i++){
-//            var_export($dump[$i]);
-//        }
-//    }
-//}
+$akvarium = array(
+    'ribu'=> array(
+        array(
+            'name' => 'Petja',
+            'size' => 'Smoll',
+            'color' => 'Zelenui',
+            'plavnik' => '4',
+
+        ),
+        array(
+            'name' => 'Gora',
+            'siza' => 'very big',
+            'color' => 'Red',
+            'plavnik' => '8',
+        ),
+        array(
+            'name' => 'Jenja',
+            'size' => 'Big',
+            'color' => 'Serui',
+            'plavnik' => '3',
+        ),
+        array(
+            'name' => 'Vova',
+            'size' => 'very smoll',
+            'color' => 'jeltui',
+            'plavnik' => '9',
+        )
+
+    ),
+    'vodorosli'=> array(
+        array(
+            'name' => 'Hren6',
+            'color' => 'Green',
+            'dlina' => '5',
+        ),
+        array (
+            'name' => 'Belka',
+            'color' => 'Red',
+            'dlina' => '2',
+        ),
+        array(
+            'name' => 'Roza',
+            'color' => 'Fioletovui',
+            'dlina' => '7',
+        ),
+        array(
+            'name' => 'Xioma',
+            'color' => 'Serui',
+            'dlina' => '4',
+        ),
+    ),
+);
+
+echo '<div align="center">Аквариум</div>';
+
+
+foreach ($akvarium as $key => $value){
+    if ($key == 'ribu'){
+        echo '<p>Рибы</p>';
+        foreach ($value as $kkey => $vvalue){
+            foreach ($vvalue as $kkkey => $vvvalue){
+                echo '<b>Рибы ' . $kkkey . ': </b> ' . $vvvalue;
+                echo '<br />';
+            }
+        }
+    }
+
+    else {
+        echo '<p>Водоросли</p>';
+        foreach ($value as $kkey => $vvalue){
+            foreach ($vvalue as $kkkey => $vvvalue){
+                echo '<b>Водоросли ' . $kkkey . ': </b> ' . $vvvalue;
+                echo '<br />';
+
+
+            }
+        }
+    }
+
+}
+
 
--- test/tema_interface.php
+++ test/tema_interface.php
@@ -6,115 +6,53 @@
  * Time: 0:53
  */
 
-
 class ribu {
-    private  $vidRiba = 'Riba';
-    private  $name = 'Boba';
-    private  $size = 12;
-    private  $color = 'Blue';
-
-    public function getVid() {
-        $this->vidRiba;
-            }
-    public function getName(){
-        $this->name;
-    }
-    public function getSize(){
-        $this->size;
-    }
-    public function getColor(){
-        $this->color;
-    }
-
-}
-class vodorosli{
-    private  $vidVodorosli = 'Vodorosli';
-    private  $nameVodorosli = 'Zelenka';
-    private  $sizeVodorosli = 23;
-    private  $colorVodorosli = 'Green';
-
-    public function getVid(){
-        $this->vidVodorosli;
-            }
-    public function getNameVodorosli(){
-        $this->nameVodorosli;
-    }
-    public function getSizeVodorosli(){
-        $this->sizeVodorosli;
-    }
-    public function getColorVodorosli(){
-        $this->colorVodorosli;
-    }
-
+    public  $color = 'Zelenui';
+    public $name = 'Vasja';
+    public $size = 'Big';
+    public $plavnik = '2';
 }
 
-
-
-    $ribu_vodorosli = array(
-        array(
-            'vidRiba' => 'riba',
-            'name' => 'Vasa',
-            'size' => 2,
-            'color' => 'Red'
-        ),
-        array(
-            'vidRiba' => 'riba',
-            'name' => 'Biba',
-            'size' => '16',
-            'color' => 'Red'
-        ),
-        array(
-            'vidRiba' => 'riba',
-            'name' => 'Lola',
-            'size' => '45',
-            'color' => 'Black'
-        ),
-        array(
-            'vidVodorosli' => 'vodorosli',
-            'name' => 'HrenZnaet4to',
-            'size' => 'Big',
-            'color' => 'Belui'
-        ),
-        array(
-            'vidVodorosli' => 'vodorosli',
-            'name' => 'Trof',
-            'size' => 'smol',
-            'color' => 'serui'
-        ),
-        array(
-            'vidVodorosli' => 'vodorosli',
-            'name' => 'Tifon',
-            'size' => 'Very Big',
-            'color' => 'Krasnui'
-        )
-
-    );
-
-
-
-    var_export($ribu_vodorosli);
-
-    foreach ($ribu_vodorosli as $key => $value){
-    echo $value, $key;
-}
-
-
-class ribuModern extends ribu {
+class ribuNasledovanie extends ribu {
+    public $color;
     public $name;
     public $size;
-    public $color;
-    public $vidRiba;
+    public $plavnik;
 
-    public function getName(){
-        return $this ->name;
-
+    public  function getColor(){
+        return $this->color;
+    }
+    public  function  getName(){
+        return $this->name;
+    }
+    public function getSize(){
+        return $this->size;
+    }
+    public  function getPlavnik(){
+        return $this->size;
     }
 
 }
-class vodorosliModern extends vodorosli{
-    public $nameVodorosli;
-    public $sizeVodorosli;
-    public $colorVodorosli;
-    public $vidVodorosli;
+
+class vodorosli {
+    public $color = 'belui';
+    public $name = 'Zelenka';
+    public $dlina = '12';
+}
+class vodorosliNasledovanie extends vodorosli {
+    public $color;
+    public $name;
+    public $dlina;
+
+    public  function getColor(){
+        return $this->color;
+    }
+    public function getName(){
+        return $this->name;
+    }
+    public function getDlina(){
+        return $this->dlina;
+    }
+
 
 }
